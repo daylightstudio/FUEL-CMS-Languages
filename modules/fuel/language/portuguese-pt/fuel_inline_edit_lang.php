ad8679cbf1d447324b6c3ead9402d698919dd9bf
adaragao <adaragao@globeflash.pt>
2013-03-14 19:14:02 +0000

    created the Portuguese-pt language

---
+++ modules/fuel/language/portuguese-pt/fuel_inline_edit_lang.php
@@ -0,0 +1,22 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Inline Edit Toolbar
+|--------------------------------------------------------------------------
+*/
+$lang['inline_edit_toggle_toolbar'] = 'Toggle toolbar display';
+$lang['inline_edit_toggle_editable'] = 'Toggle editable areas';
+$lang['inline_edit_toggle_publish'] = 'Toggle the pages publish status';
+$lang['inline_edit_toggle_cache'] = 'Toggle page cache settings';
+
+$lang['inline_edit_tools'] = 'Ferramentas...';
+$lang['inline_edit_other_pages'] = 'Outras páginas...';
+$lang['inline_edit_layout'] = 'Layout';
+
+$lang['inline_edit_back_to_admin'] = 'Voltar á Administração';
+$lang['inline_edit_logout'] = 'Sair';
+$lang['inline_edit_logout_title'] = 'Sair da Administração do FUEL';
+$lang['inline_edit_page_not_published'] = 'Esta página não está publicada';
+
+/* End of file fuel_inline_edit_lang.php */
+/* Location: ./modules/fuel/language/english/fuel_inline_edit_lang.php */
---
+++ modules/fuel/language/portuguese-pt/model_lang.php
@@ -0,0 +1,24 @@
+<?php 
+// model specific error messages
+$lang['error_val_empty_or_already_exists'] = 'The %1s is empty or already exists.';
+$lang['error_location_parents_match'] = 'Location and parents can\'t match.';
+$lang['error_in_readonly_mode'] = 'The %1s is in readonly mode and can\'t modify data.';
+
+// standard model errors
+$lang['error_required_fields'] = "Please fill out the required field '%1s'";
+$lang['error_valid_email'] = 'Please enter in a valid email address.';
+$lang['error_valid_phone'] = 'Please enter in a valid phone number.';
+$lang['error_method_does_not_exist'] = "Método %1s não eiste.";
+$lang['error_class_does_not_exist'] = "Classe %1s não existe.";
+
+$lang['error_value_exceeds_length'] = 'Value exceeds required length for %1s.';
+$lang['error_not_number'] = 'Value needs to be a number for %1s.';
+$lang['error_invalid_date'] = 'Invalid date for %1s';
+$lang['error_invalid_time'] = 'Invalid time for %1s';
+$lang['error_invalid_year'] = 'Invalid year for %1s';
+$lang['error_invalid_generic'] = 'Valor inválido para %1s';
+$lang['error_could_not_find_record_class'] = 'Could not find record class to create for %1s.';
+$lang['error_invalid_custom_class'] = 'Invalid custom class associated with the field %1s.';
+
+/* End of file model_lang.php */
+/* Location: ./modules/fuel/language/english/model_lang.php */
