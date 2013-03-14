0763ed8dd0be53a49fbf7374b578722f32e7076d
adaragao <adaragao@globeflash.pt>
2013-03-14 22:22:39 +0000

    Completed translation file

--- modules/fuel/language/portuguese-pt/model_lang.php
+++ modules/fuel/language/portuguese-pt/model_lang.php
@@ -1,24 +1,24 @@
 <?php 
 // model specific error messages
-$lang['error_val_empty_or_already_exists'] = 'The %1s is empty or already exists.';
-$lang['error_location_parents_match'] = 'Location and parents can\'t match.';
-$lang['error_in_readonly_mode'] = 'The %1s is in readonly mode and can\'t modify data.';
+$lang['error_val_empty_or_already_exists'] = ' %1s está vazio ou já existe.';
+$lang['error_location_parents_match'] = 'Localização e  parentes não podem coincidir..';
+$lang['error_in_readonly_mode'] = '%1s é apenas de leitura, não pode editar dados.';
 
 // standard model errors
-$lang['error_required_fields'] = "Please fill out the required field '%1s'";
-$lang['error_valid_email'] = 'Please enter in a valid email address.';
-$lang['error_valid_phone'] = 'Please enter in a valid phone number.';
-$lang['error_method_does_not_exist'] = "Método %1s não eiste.";
-$lang['error_class_does_not_exist'] = "Classe %1s não existe.";
+$lang['error_required_fields'] = "Por favor, preencha os campos obrigatórios '%1s'";
+$lang['error_valid_email'] = 'Por favor, nsira um endereço de email válido.';
+$lang['error_valid_phone'] = 'Por favor, insira um número de telefone válido.';
+$lang['error_method_does_not_exist'] = "O método %1s não existe.";
+$lang['error_class_does_not_exist'] = "A classe %1s não existe.";
 
-$lang['error_value_exceeds_length'] = 'Value exceeds required length for %1s.';
-$lang['error_not_number'] = 'Value needs to be a number for %1s.';
-$lang['error_invalid_date'] = 'Invalid date for %1s';
-$lang['error_invalid_time'] = 'Invalid time for %1s';
-$lang['error_invalid_year'] = 'Invalid year for %1s';
+$lang['error_value_exceeds_length'] = 'O valor excede o comprimento permitido para %1s.';
+$lang['error_not_number'] = 'O valor precisa ser um número para %1s.';
+$lang['error_invalid_date'] = 'Data inválida para %1s';
+$lang['error_invalid_time'] = 'Hora inválida para %1s';
+$lang['error_invalid_year'] = 'Ano inválida para %1s';
 $lang['error_invalid_generic'] = 'Valor inválido para %1s';
-$lang['error_could_not_find_record_class'] = 'Could not find record class to create for %1s.';
-$lang['error_invalid_custom_class'] = 'Invalid custom class associated with the field %1s.';
+$lang['error_could_not_find_record_class'] = ' Não foi possível encontrar a classe de registro para criar  %1s.';
+$lang['error_invalid_custom_class'] = 'Inválida a classe personalizada associada ao campo %1s.';
 
 /* End of file model_lang.php */
 /* Location: ./modules/fuel/language/english/model_lang.php */
