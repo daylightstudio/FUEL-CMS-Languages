5fc1fe39d89cbdfee751f73ba78dd905ea0755dc
David McReynolds <dave@thedaylightstudio.com>
2014-12-04 09:32:20 -0800

    v1.2 russian updates

---
+++ modules/fuel/language/russian/model_lang.php
@@ -0,0 +1,23 @@
+<?php 
+// model specific error messages
+$lang['error_val_empty_or_already_exists'] = '%1s пуста или уже существует.';
+$lang['error_in_readonly_mode'] = '%1s в режиме только для чтения и не может изменять данные.';
+
+// standard model errors
+$lang['error_required_fields'] = "Пожалуйста, заполните необходимое поле '%1s'";
+$lang['error_valid_email'] = 'Пожалуйста, введите в корректный адрес электронной почты.';
+$lang['error_valid_phone'] = 'Пожалуйста, введите корректный номер телефона.';
+$lang['error_method_does_not_exist'] = "Метод %1s не существует.";
+$lang['error_class_does_not_exist'] = "Класс %1s не существует.";
+
+$lang['error_value_exceeds_length'] = 'Значение превышает допустимую длину %1s.';
+$lang['error_not_number'] = 'Значение %1s должно быть числом.';
+$lang['error_invalid_date'] = 'Неверный формат даты %1s';
+$lang['error_invalid_time'] = 'Неверный формат время %1s';
+$lang['error_invalid_year'] = 'Неверный формат года %1s';
+$lang['error_invalid_generic'] = 'Неверный формат %1s';
+$lang['error_could_not_find_record_class'] = 'Не удалось найти запись класса для создания %1s.';
+$lang['error_invalid_custom_class'] = 'Неверный пользовательский класс, связанный с полем %1s.';
+
+/* End of file model_lang.php */
+/* Location: ./modules/fuel/language/english/model_lang.php */
