8ce412f993cc1d0ef5e0449e8ab6b747e5c9948b
Denny Schwender <denny.schwender@gmail.com>
2016-02-19 00:54:50 +0100

    Upload italian traductions
    Upload for the main traduction in italian

---
+++ modules/fuel/language/italian/fuel_inline_edit_lang.php
@@ -0,0 +1,24 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Inline Edit Toolbar
+|--------------------------------------------------------------------------
+*/
+$lang['inline_edit_toggle_toolbar'] = 'Modifica visibilità toolbar';
+$lang['inline_edit_toggle_editable'] = 'Modifica aree editabili';
+$lang['inline_edit_toggle_publish'] = 'Modifica lo stato di pubblicazione della pagina';
+$lang['inline_edit_toggle_cache'] = 'Modifica lo stato di cache della pagina';
+
+$lang['inline_edit_tools'] = 'Strumenti...';
+$lang['inline_edit_other_pages'] = 'Altre pagine...';
+$lang['inline_edit_layout'] = 'Modello';
+
+$lang['inline_edit_back_to_admin'] = 'Indietro all\'admin';
+$lang['inline_edit_logout'] = 'Logout';
+$lang['inline_edit_logout_title'] = 'Logout dell\'admin FUEL';
+$lang['inline_edit_login'] = 'Login';
+$lang['inline_edit_login_title'] = 'Login dell\'admin FUEL';
+$lang['inline_edit_page_not_published'] = 'La pagina non è pubblicata';
+
+/* End of file fuel_inline_edit_lang.php */
+/* Location: ./modules/fuel/language/italian/fuel_inline_edit_lang.php */
